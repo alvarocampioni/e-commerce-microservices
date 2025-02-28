83a4577db06ed120131d26e83f1c01426189ad6f
alvaro_rc <109553591+alvarocampioni@users.noreply.github.com>
2025-02-28 17:21:58 -0300

    Reapply "including product kafka consumer tests"
    This reverts commit 5d157ddda42edc7d53c871c5ab561de18a5e4cd1.

--- product-service/src/main/java/com/ms/product_service/dto/OrderProduct.java
+++ product-service/src/main/java/com/ms/product_service/dto/OrderProduct.java
@@ -1,6 +1,4 @@
 package com.ms.product_service.dto;
 
-import java.math.BigDecimal;
-
-public record OrderProduct(String orderId, String customerId, String productId, BigDecimal price, int amount) {
+public record OrderProduct(String productId, int amount) {
 }
