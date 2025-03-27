98f4a39cd9bcebb2e85ffaa7b52980752267ae43
alvaro_rc <109553591+alvarocampioni@users.noreply.github.com>
2025-03-27 16:55:41 -0300

    Update PaymentServiceApplicationTests.java

--- payment-service/src/test/java/com/ms/payment_service/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/ms/payment_service/PaymentServiceApplicationTests.java
@@ -98,7 +98,6 @@ class PaymentServiceApplicationTests {
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
 		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 	}
 
 	@BeforeEach
